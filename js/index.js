7ed62b9cd3d729f5aa0ecebf67fb5865f7db656e
SC1314520 <blc0000421@gmail.com>
2022-12-14 15:31:35 +0800

    s

--- js/index.js
+++ js/index.js
@@ -19,27 +19,33 @@ window.onload=function(){
                 this.open=false;
             },
             submit(){
-                this.open=false;
-                alert("提交成功，將於 2024.12.04 寄件於登記信箱中。")
-                console.log("send");
-
-                // 資料創建
-                let formdata = new FormData();
-                formdata.append('user', this.user);
-                formdata.append('password', this.mail);
-                formdata.append('content', this.content);
-
-                var config = {
-                method: 'POST',
-                body: formdata,
-                redirect: 'follow'
-                };
-                fetch("https://script.google.com/macros/s/AKfycbzzvcV7pFVc9tAHk5-XFRntANmPqIz4ZTMHHfiF9RXiTmKqapDA3J29FlI4DmO_-D3Z/exec", config)
-                .then(response => response.text())
-                .then(result => {
-                    console.log(result);
-                })
-                .catch(error => console.log('error', error));
+                if(confirm("確認提交？")){
+                    this.open=false;
+                    alert("提交成功，將於 2024.12.04 寄件於登記信箱中。")
+                    console.log("send");
+    
+                    // 資料創建
+                    let formdata = new FormData();
+                    formdata.append('user', this.user);
+                    formdata.append('password', this.mail);
+                    formdata.append('content', this.content);
+    
+                    var config = {
+                    method: 'POST',
+                    body: formdata,
+                    redirect: 'follow'
+                    };
+                    fetch("https://script.google.com/macros/s/AKfycbzzvcV7pFVc9tAHk5-XFRntANmPqIz4ZTMHHfiF9RXiTmKqapDA3J29FlI4DmO_-D3Z/exec", config)
+                    .then(response => response.text())
+                    .then(result => {
+                        console.log(result);
+                    })
+                    .catch(error => console.log('error', error));
+                }
+                else{
+                    alert("取消提交")
+                }
+                
             }
         },
         created(){
